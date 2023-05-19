7b9517ff4722f09270f999f8bddff9b6d4f5902f
asylbekovatilee <asylbekovanasyikat@gmail.com>
2023-05-19 11:42:57 +0600

    .

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
 from django.shortcuts import render
 
+from .models import Product
+from .serializers import ProductSerializer
+
 # Create your views here.
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
