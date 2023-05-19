1ea8d2d59f6da729ebfe36a3f4bfc71af88f0f2d
SannaRGAI <saltanat9000@gmail.com>
2023-05-19 11:13:16 +0600

    models

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    image = models.ImageField(upload_to='products', null=True, blank=True)
+    # image = models.ImageField(upload_to='products', null=True, blank=True)
     
     def __str__(self) -> str:
         return self.title
